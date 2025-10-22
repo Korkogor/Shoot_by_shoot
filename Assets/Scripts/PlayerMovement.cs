db0a8ec410e201f51666c737d26fec538815164e
Daniil <korkogor@gmail.com>
2025-10-22 19:18:19 +0300

    Initial Unity project commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {transform.Translate(0, 0, speed);}
+        if (Input.GetKey(KeyCode.S)){transform.Translate(0, 0, -speed);}
+        if (Input.GetKey(KeyCode.A)){transform.Translate(-speed, 0, 0);}
+        if (Input.GetKey(KeyCode.D)){transform.Translate(speed, 0, 0);}
+        
+        float mouseX = Input.GetAxis("Mouse X"); // считывание горизонтального перемещения мыши
+        transform.Rotate(0, mouseX * rotationSpeed, 0); //горизонтальный поворот объекта на основе движения мыши
+    }
+}

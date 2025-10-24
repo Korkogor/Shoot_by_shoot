561543c8705094a7aa75412aef3920706cbeaf16
Daniil <korkogor@gmail.com>
2025-10-24 14:53:33 +0300

    Test commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,12 @@ public class PlayerMovement : MonoBehaviour
     public float rotationSpeed;
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {transform.Translate(0, 0, speed);}
-        if (Input.GetKey(KeyCode.S)){transform.Translate(0, 0, -speed);}
-        if (Input.GetKey(KeyCode.A)){transform.Translate(-speed, 0, 0);}
-        if (Input.GetKey(KeyCode.D)){transform.Translate(speed, 0, 0);}
+        if (Input.GetKey(KeyCode.W)) {transform.Translate(0, 0, speed * Time.deltaTime);}
+        if (Input.GetKey(KeyCode.S)){transform.Translate(0, 0, -speed * Time.deltaTime);}
+        if (Input.GetKey(KeyCode.A)){transform.Translate(-speed * Time.deltaTime, 0, 0);}
+        if (Input.GetKey(KeyCode.D)){transform.Translate(speed * Time.deltaTime, 0, 0);}
         
         float mouseX = Input.GetAxis("Mouse X"); // считывание горизонтального перемещения мыши
-        transform.Rotate(0, mouseX * rotationSpeed, 0); //горизонтальный поворот объекта на основе движения мыши
+        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //горизонтальный поворот объекта на основе движения мыши
     }
 }

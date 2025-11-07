ae68f06d1ce717c89e5981ba630f82ad096f15b4
Daniil <korkogor@gmail.com>
2025-11-07 15:21:04 +0300

    Reformed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    private Vector2 movementValue;
+    private float lookValue;
 
-    void Start()
+    void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
+
+    public void OnMove(InputValue value)
+    {
+        movementValue = value.Get<Vector2>() * speed * Time.deltaTime;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<float>() * rotationSpeed * Time.deltaTime;
+    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {transform.Translate(0, 0, speed * Time.deltaTime);}
-        if (Input.GetKey(KeyCode.S)){transform.Translate(0, 0, -speed * Time.deltaTime);}
-        if (Input.GetKey(KeyCode.A)){transform.Translate(-speed * Time.deltaTime, 0, 0);}
-        if (Input.GetKey(KeyCode.D)){transform.Translate(speed * Time.deltaTime, 0, 0);}
-        
-        float mouseX = Input.GetAxis("Mouse X"); // считывание горизонтального перемещения мыши
-        transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //горизонтальный поворот объекта на основе движения мыши
+        transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime );
+        transform.Rotate(Vector3.up, lookValue * Time.deltaTime);
+        // if (Input.GetKey(KeyCode.W)) {transform.Translate(0, 0, speed * Time.deltaTime);}
+        // if (Input.GetKey(KeyCode.S)){transform.Translate(0, 0, -speed * Time.deltaTime);}
+        // if (Input.GetKey(KeyCode.A)){transform.Translate(-speed * Time.deltaTime, 0, 0);}
+        // if (Input.GetKey(KeyCode.D)){transform.Translate(speed * Time.deltaTime, 0, 0);}
+        //
+        // float mouseX = Input.GetAxis("Mouse X"); // считывание горизонтального перемещения мыши
+        // transform.Rotate(0, mouseX * rotationSpeed * Time.deltaTime, 0); //горизонтальный поворот объекта на основе движения мыши
         
     }
     
